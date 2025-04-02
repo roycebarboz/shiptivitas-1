f871065e0c1fc3daa4419d9a8459a9fc6999722e
Royce <roycebarboz@gmail.com>
2025-04-02 15:21:39 -0400

    documentation 2

--- src/Board.js
+++ src/Board.js
@@ -22,72 +22,72 @@ export default class Board extends React.Component {
     };
   }
 
-  // componentDidMount() {
+  componentDidMount() {
 
-  //   const allClients = this.getClients().map(client => ({ ...client, status: 'backlog' }));
-  //   this.setState({
-  //     clients: {
-  //       backlog: allClients,
-  //       inProgress: [],
-  //       complete: [],
-  //     },
-  //   });
+    const allClients = this.getClients().map(client => ({ ...client, status: 'backlog' }));
+    this.setState({
+      clients: {
+        backlog: allClients,
+        inProgress: [],
+        complete: [],
+      },
+    });
 
-  //   this.drake = Dragula([
-  //     this.swimlanes.backlog.current,
-  //     this.swimlanes.inProgress.current,
-  //     this.swimlanes.complete.current,
-  //   ]);
-  //   this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
-  // }
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current,
+    ]);
+    this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
+  }
   
-  // componentWillUnmount() {
-  //   this.drake.remove();
-  // }
+  componentWillUnmount() {
+    this.drake.remove();
+  }
   
-  // /**
-  //  * Change the status of client when a Card is moved
-  //  */
-  // updateClient(el, target, _, sibling) {
-  //   // Reverting DOM changes from Dragula
-  //   this.drake.cancel(true);
+  /**
+   * Change the status of client when a Card is moved
+   */
+  updateClient(el, target, _, sibling) {
+    // Reverting DOM changes from Dragula
+    this.drake.cancel(true);
   
-  //   // Find out which swimlane the Card was moved to
-  //   let targetSwimlane = 'backlog';
-  //   if (target === this.swimlanes.inProgress.current) {
-  //     targetSwimlane = 'in-progress';
-  //   } else if (target === this.swimlanes.complete.current) {
-  //     targetSwimlane = 'complete';
-  //   }
+    // Find out which swimlane the Card was moved to
+    let targetSwimlane = 'backlog';
+    if (target === this.swimlanes.inProgress.current) {
+      targetSwimlane = 'in-progress';
+    } else if (target === this.swimlanes.complete.current) {
+      targetSwimlane = 'complete';
+    }
   
-  //   // Create a new clients array
-  //   const clientsList = [
-  //     ...this.state.clients.backlog,
-  //     ...this.state.clients.inProgress,
-  //     ...this.state.clients.complete,
-  //   ];
-  //   const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
-  //   const clientThatMovedClone = {
-  //     ...clientThatMoved,
-  //     status: targetSwimlane,
-  //   };
+    // Create a new clients array
+    const clientsList = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+    const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
+    const clientThatMovedClone = {
+      ...clientThatMoved,
+      status: targetSwimlane,
+    };
   
-  //   // Remove ClientThatMoved from the clientsList
-  //   const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
+    // Remove ClientThatMoved from the clientsList
+    const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
   
-  //   // Place ClientThatMoved just before the sibling client, keeping the order
-  //   const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
-  //   updatedClients.splice(index === -1 ? updatedClients.length : index , 0, clientThatMovedClone);
+    // Place ClientThatMoved just before the sibling client, keeping the order
+    const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+    updatedClients.splice(index === -1 ? updatedClients.length : index , 0, clientThatMovedClone);
   
-  //   // Update React state to reflect changes
-  //   this.setState({
-  //     clients: {
-  //       backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
-  //       inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
-  //       complete: updatedClients.filter(client => client.status && client.status === 'complete'),
-  //     }
-  //   });
-  // }
+    // Update React state to reflect changes
+    this.setState({
+      clients: {
+        backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
+        inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
+        complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+      }
+    });
+  }
   
 
   getClients() {
